d326054078112e213bdc4fc511098c9bb3acc084
Sidorcenko <artjoms.sidorcenko@sk.lvt.lv>
2024-04-16 12:09:50 +0300

    Pedēja versija

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -2,20 +2,8 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Kaulins {
-<<<<<<< HEAD
 
-	public static void main(String[] args) {
-		int sk, r;
-		Random rand = new Random();
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Ievadi cik reizes mest kaulinu?");
-		r = scan.nextInt();
-		for(int i=0;i<r;i++) {
-		sk = rand.nextInt(6)+1;
-		System.out.println("Tu uzmeti: "+sk);
-		}
-		scan.close();
-=======
+
 	
 	static void mestKaulinu(int reizes) {
 		int sk;
@@ -26,8 +14,11 @@ public class Kaulins {
 		}
 	}
 	public static void main(String[] args) {
-		mestKaulinu(5);
->>>>>>> idejuZars
+		int r;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Ievadi cik reizes mest kaulinu?");
+		r = scan.nextInt();
+		mestKaulinu(r);
 	}
 
 }
