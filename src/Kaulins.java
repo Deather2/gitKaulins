086cb27448869d90d2917cdc8508011ba3839ccc
Sidorcenko <artjoms.sidorcenko@sk.lvt.lv>
2024-04-16 11:53:42 +0300

    Ieviesta metode

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,13 +1,15 @@
 import java.util.Random;
 
 public class Kaulins {
-
-	public static void main(String[] args) {
+	
+	static void mestKaulinu() {
 		int sk;
 		Random rand = new Random();
 		sk = rand.nextInt(6)+1;
 		System.out.println("Tu uzmeti: "+sk);
-
+	}
+	public static void main(String[] args) {
+		mestKaulinu();
 	}
 
 }
