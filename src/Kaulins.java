29a09ec7a52d4aec1bc9d4c48e659afaf80ae3af
Sidorcenko <artjoms.sidorcenko@sk.lvt.lv>
2024-04-16 11:57:22 +0300

    Iespēja mest kaulinu vairākas reizes

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -2,14 +2,16 @@ import java.util.Random;
 
 public class Kaulins {
 	
-	static void mestKaulinu() {
+	static void mestKaulinu(int reizes) {
 		int sk;
 		Random rand = new Random();
+		for(int i=0;i<reizes;i++) {
 		sk = rand.nextInt(6)+1;
 		System.out.println("Tu uzmeti: "+sk);
+		}
 	}
 	public static void main(String[] args) {
-		mestKaulinu();
+		mestKaulinu(5);
 	}
 
 }
