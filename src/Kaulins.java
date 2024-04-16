70886b395461a88d59d3477fb53e7ce8581b054a
Sidorcenko <artjoms.sidorcenko@sk.lvt.lv>
2024-04-16 12:03:04 +0300

    Vairākkārtīga kauliņu mešana

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,13 +1,19 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Kaulins {
 
 	public static void main(String[] args) {
-		int sk;
+		int sk, r;
 		Random rand = new Random();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Ievadi cik reizes mest kaulinu?");
+		r = scan.nextInt();
+		for(int i=0;i<r;i++) {
 		sk = rand.nextInt(6)+1;
 		System.out.println("Tu uzmeti: "+sk);
-
+		}
+		scan.close();
 	}
 
 }
