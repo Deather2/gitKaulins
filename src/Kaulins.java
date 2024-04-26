a180d5a10d6bf8b16c4b05a4a6f331e06a51d03a
Sidorcenko <artjoms.sidorcenko@sk.lvt.lv>
2024-04-26 08:48:18 +0300

    Programma atrod lielāko no uzkritušajiem skaitļiem

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -6,18 +6,23 @@ public class Kaulins {
 
 	
 	static void mestKaulinu(int reizes) {
-		int sk;
+		int sk,lielakais=0;
 		Random rand = new Random();
 		for(int i=0;i<reizes;i++) {
 		sk = rand.nextInt(6)+1;
 		System.out.println("Tu uzmeti: "+sk);
+		if(lielakais<sk)
+			lielakais=sk;
 		}
+		System.out.println("Lielākais no visiem metienim: "+lielakais);
 	}
 	public static void main(String[] args) {
 		int r;
 		Scanner scan = new Scanner(System.in);
+		do {
 		System.out.println("Ievadi cik reizes mest kaulinu?");
 		r = scan.nextInt();
+		}while(r<1);
 		mestKaulinu(r);
 		scan.close();
 	}
